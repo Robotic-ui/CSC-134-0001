e50fa92065b2c579eadf6ad6a8d955dcdc2e39bf
Sebastian Rusbacky <84217158+Robotic-ui@users.noreply.github.com>
2024-08-26 14:54:37 +0000

    new file

---
+++ M1HW1_Rusbacky.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+// M1HW1
+// Sebastian Rusbacky
+// 8/25/2024
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    string movieName = "WarGames";
+    int movieYear = 1983;
+    double grossIncome = 79.567667;
+
+    cout << "The movie " << movieName << " came out in " << movieYear << " and had a gross income of about $" << grossIncome << " billion dollars, " << "ranking as the fifth highest in the United States and Canada. \n";
+    cout << "Qoutes\n";
+    cout << "Greetings Dr. Falken Shall we play a game? - Joshua\n";
+    cout << "Goddammit, I'd piss on a spark plug if I thought it'd do any good! - Gen Beringer\n";
+    cout << "- Mr. Liggett: Lightman, maybe you could tell us who first suggested the idea of reproduction without sex. - David: Umm... Your wife?\n";
+    cout << "The only winning move is not to play\n";
+    cout << "Dr Falken was supposed to be played by John Lennon heavly based on Stepen Hawking, Also this film didn't have a hard set script most of it was improvised on the spot\n";
+    cout << "It is said that President Reagan was a strong fan of the film so much so that he stopped a meeting regarding ongoing nuclear negotiations with the Russians to give everyone on the room a full breakdown of the plot. \nWhen he was fished he asked General John W. Vessey Jr to look into how plausible to film was, he determined that WarGames actually was a prescient indicator of a rising threat in the new world of cybersecurity. More than a year later Reagan signed a classified directive titled National Policy on the Telecommunications and Automated Infomation Systems Security. It was the first ever presidential directive given by a president all because he saw a kid who wanted to play some video games";
+}
