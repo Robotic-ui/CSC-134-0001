600571b6b750535036cc467395129d2e157e64c8
Sebastian Rusbacky <84217158+Robotic-ui@users.noreply.github.com>
2024-08-28 15:21:08 +0000

    new file M2T1

--- module2/M2T1_Rusbacky.cpp
+++ module2/M2T1_Rusbacky.cpp
@@ -2,12 +2,23 @@
 // M2T1
 // Sebastian Rusbacky
 // 8/28/2024
+// simple receipt output
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world" << endl;
-    return 0;
+    double mealPrice = 5.99;
+    double taxRate = 0.08;
+    double taxAmount;
+    double total;
+
+    taxAmount = taxRate * mealPrice;
+    total += taxAmount;
+
+    cout << "Thank you for coming and dining with us!\n" << endl;
+    cout << "Your meal total is $" << mealPrice << endl;
+    cout << "The tax amount is $" << taxAmount << endl;
+    cout << "Your total is $" << total << endl;
 }
