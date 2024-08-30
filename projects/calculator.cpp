c18809eb12e1289068c3af1fcb79ed31e82ec408
Sebastian Rusbacky <84217158+Robotic-ui@users.noreply.github.com>
2024-08-29 21:03:11 -0400

    new project calc

---
+++ projects/calculator.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main()
+{
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << "********** CALCULATOR **********"\n << endl;
+
+    std::cout << "Enter either (+ - * or /): ";
+    std::cin >> op;
+
+    std::cout << "Enter #1: ";
+    std::cin >> num1;
+
+    std::cout << "Enter #2: ";
+    std::cin >> num2;
+    
+    switch(op) 
+    {
+        case '+':
+            result = num1 + num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '-':
+            result = num1 - num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '*':
+            result = num1 * num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '/':
+            result = num1 / num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        default:
+            std::cout << "invalid response!\n";
+            break;
+    }
+
+    std::cout << "********** CALCULATOR **********"\n; << endl;
+
+    return 0;
+}
