db0a04af5b904182b0cb767d96a851f03192a21b
Sebastian Rusbacky <84217158+Robotic-ui@users.noreply.github.com>
2024-12-03 04:31:25 -0500

    updated basicFunctions.cpp

---
+++ projects/basicFunctions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void exampleFunction(); // (declaration) NOTE: fuction cannot be declared after the main function ERROR!
+{
+    // definition goes here
+}
+
+int main () 
+{
+    myfunction() // called function
+    return 0;
+}
+
+void myfunction()
+{
+    cout << "I just got executed";
+}
